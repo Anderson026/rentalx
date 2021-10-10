1ba90753f19250b49d48f4d41810d0d7b273d2c3
Anderson026 <anderson-ferreira22@hotmail.com>
2021-10-10 15:37:58 -0300

    Inserindo os dados do upload no repositório

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -2,22 +2,53 @@
 import fs from "fs";
 // importando o csv parse
 import csvParse from "csv-parse";
+import { ICategoriesrepository } from "../../repositories/ICategoriesRepository";
+// interface para utilizar o nome e a descrição do conteúdo do arquivo csv importado
+interface IImportCategory {
+  name: string;
+  description: string;
+}
 
 class ImportCategoryUseCase {
 
-  execute(file: Express.Multer.File): void {
-    
-    // cria um stream de leitura de arquivo e passa o caminho onde está o arquivo
-    const stream = fs.createReadStream(file.path);
-    // importando a função de csv parse para uma variável
-    const parseFile = csvParse();
-    // utilizando o pipe do stream para enviar o arquivos divido em pedaços para uma pasta que será configurada
-    stream.pipe(parseFile);
+  constructor(private categoriesRepository: ICategoriesrepository) {
 
-    parseFile.on("data", async(line) => {
-      console.log(line);
-      
-    })
+  }
+  // método que faz a leitura dos arquivos enviados
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    // retornando uma promisse
+    return new Promise((resolve, reject) => {
+       // cria um stream de leitura de arquivo e passa o caminho onde está o arquivo
+      const stream = fs.createReadStream(file.path);
+      // criando o array de categorias
+      const categories: IImportCategory[] = [];
+      // importando a função de csv parse para uma variável
+      const parseFile = csvParse();
+      // utilizando o pipe do stream para enviar o arquivos divido em pedaços para uma pasta que será configurada
+      stream.pipe(parseFile);
+
+      parseFile.on("data", async(line) => {
+        // desestruturando o nome e a description
+        const [name, description] = line;
+        // adiciona o nome e descrição dentro do array
+        categories.push({
+          name,
+          description,
+        });
+      })
+      .on("end", () => {
+        resolve(categories);
+      }).on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    // faz a chamado de método loadCategories, passando o arquivo importado
+    const categories = await this.loadCategories(file);
+    console.log(categories);
+    
   }
 }
 
--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,10 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+// instanciando o repositório de categorias
+const categoriesRepository = CategoriesRepository.getInstance();
 // instanciando o caso de uso
-const importCategoryUseCase = new ImportCategoryUseCase();
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 // instanciando o controller
 const importCategoryController = new ImportCategoryController(importCategoryUseCase);
 
