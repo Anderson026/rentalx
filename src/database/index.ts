f1c9965dfb0b26dd8647d3123a45a73a589aff5e
Anderson026 <anderson-ferreira22@hotmail.com>
2021-11-13 19:39:10 -0300

    Criando a migration de cadastro de categorias e configurando as entities na classe de categoria para adicionar os dados no banco de dados

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,13 @@
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-import { createConnection } from "typeorm";
-// criando a conexão com o banco de dados
-createConnection();
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then(options => {
+  const newOptions = options as IOptions;
+  newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options,
+  });
+});
--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,10 +1,20 @@
 // importando a lib uuid para gerar ids
 import { v4 as uuidV4 } from "uuid";
+// importando a entitie para criação dos dados no banco de dados com o typeorm
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 // criando a classe de categoria
+@Entity("categories")
 class Category {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+  
+  @CreateDateColumn()
   created_at: Date;
   // método construtor
   constructor() {
