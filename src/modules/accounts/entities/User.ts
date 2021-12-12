bf543b023cadaea716c451c9c9d6197cf4fb20de
Anderson026 <anderson-ferreira22@hotmail.com>
2021-12-12 16:07:33 -0300

    Criando a migration de ususários e a sua entidade

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,37 @@
+import { v4 as uuidV4 } from "uuid";
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
+// adicionando as entidades para salvar os dados no banco de dados
+@Entity("users")
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  driver_licence: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { User };
