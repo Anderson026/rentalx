7ab0c11bb53676e9378f9b1864e06bd996fa2fef
Anderson026 <anderson-ferreira22@hotmail.com>
2022-01-02 15:41:24 -0300

    Criando o teste de create category

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,18 +1,54 @@
 
-// descrição do teste
-describe("Criar categoria", () => {
-  // função que vai testar a implementação
-  it("Espero que 2 + 2 seja 4", () => {
-    const soma = 2 + 2;
-    const resultado = 4;
-    // espera o resultado correto
-    expect(soma).toBe(resultado);
-  });
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-  it("Espero que 2 + 2 não seja 4", () => {
-    const soma =  2 + 2;
-    const resultado = 5;
 
-    expect(soma).not.toBe(resultado);
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () => {
+
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory);
+  })
+  // verifica se a categoria foi criada
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category description Test",
+    }
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name);
+
+    console.log(categoryCreated);
+    
+    expect(categoryCreated).toHaveProperty("id");
   });
-})
+  // verifica se já existe nome na categoria
+  it("should not be able to create a new category with name exists", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category Test",
+        description: "Category description Test",
+      }
+  
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+  
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+
+  });
+});
