110a3c9b0e8423f69e871e57e91944c85afa0dc8
Anderson026 <anderson-ferreira22@hotmail.com>
2022-02-22 21:55:30 -0300

    Controller de devolução de carro

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -1,4 +1,4 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
@@ -11,6 +11,7 @@ interface IRquest {
   user_id: string;
 }
 
+@injectable()
 class DevolutionRentalUseCase {
 
   constructor(
@@ -24,7 +25,7 @@ class DevolutionRentalUseCase {
 
   async execute({id, user_id}: IRquest): Promise<Rental> {
     const rental = await this.rentalsRepository.findById(id);
-    const car = await this.carsRepository.findById(id);
+    const car = await this.carsRepository.findById(rental.car_id);
 
     const minimum_daily = 1;
 
