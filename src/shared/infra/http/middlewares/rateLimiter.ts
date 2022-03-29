b059397adced17abf296a505fdd1129a4933d35d
Anderson026 <anderson-ferreira22@hotmail.com>
2022-03-29 05:09:15 -0300

    Ajustando o rateLimiter para conectar e desconectar no banco de dados redis

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -4,15 +4,19 @@ import redis from "redis";
 import { AppError } from "@shared/errors/AppError";
 
 const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
+  legacyMode: true,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    sessionTimeout: 20,
+  },
 });
 
 const limiter = new RateLimiterRedis({
   storeClient: redisClient,
   keyPrefix: 'rateLimiter',
   points: 10,
-  duration: 1,
+  duration: 5,
 })
 
 export default async function rateLimiter(
@@ -21,9 +25,12 @@ export default async function rateLimiter(
   next: NextFunction  
 ): Promise<void> {
   try {
+    await redisClient.connect();
     await limiter.consume(request.ip);
     return next();
   } catch (err) {
     throw new AppError("Too many requests", 429);
+  } finally {
+    await redisClient.disconnect();
   }
 }
